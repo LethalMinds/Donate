e620207db7fd5bb502cf3058e7bfea08568daf8f
Nishok <nishok.n.m@gmail.com>
2016-08-13 22:03:32 -0400

    Node > 6 changes Proper error messages (404) New gradle build plugin

--- Node/app.js
+++ Node/app.js
@@ -66,10 +66,8 @@ passport.deserializeUser(Account.deserializeUser());
 mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res) {
+  res.status(404).send("Not Found Error");
 });
 
 // error handlers
